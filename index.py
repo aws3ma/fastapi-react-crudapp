9128772b4dce6f4bf43961d56676c5a3f81b5627
oussema <oussematrabelsi@sfax.r-iset.tn>
2022-09-10 11:21:50 +0100

    add cors middleware

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+client_apps = ["http://localhost:3000"]
 app = FastAPI()
 app.include_router(student_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
